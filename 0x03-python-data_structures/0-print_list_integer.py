e40d03d471a46b523145e3d0438a4e5e64a8c2ce
Timothy Ofie <timzblue@gmail.com>
2024-01-08 15:12:12 -0800

    task1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
-def print_list_integer(my_list=None):
-    if my_list is None:
-        my_list = []
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
     for i in my_list:
         print("{:d}".format(i))
